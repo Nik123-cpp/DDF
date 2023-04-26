b6b78f82067c0275f813aa130436f5f3077a3dcc
pranav-159 <cs20btech11018@iith.ac.in>
2023-04-26 09:10:39 +0530

    added my requests api for faculty

--- Backend/controller/AllrequestController.js
+++ Backend/controller/AllrequestController.js
@@ -45,12 +45,38 @@ exports.public_request = (req, res, next) => {
 }
 
 //get all personal requests of a particular person
-exports.personal_request = (req, res, next) => {
+exports.faculty_all_request = (req, res, next) => {
     console.log(req.params.email_id)
     Profile.findOne({ email_address: req.params.email_id })
         .exec((err, profile) => {
             console.log(profile._id)
             Request.find({ $or: [{ requestType: "Personal", faculty: profile._id }, { requestType: "Public" }] })
+                .populate('faculty')
+                .lean()
+                .sort({ created: -1 })
+                .exec((err, request) => {
+                    if (err) {
+                        res.status(500).json({ error: err });
+                        next(err);
+                    }
+                    request.forEach(req => {
+                        req.faculty_name = req.faculty.username;
+                        req.email_address = req.faculty.email_address;
+                        delete req.faculty;
+                    });
+                    console.log(request);
+                    res.status(200).json(request);
+                });
+        });
+}
+
+//get all personal requests of a particular person
+exports.faculty_my_requests = (req, res, next) => {
+    console.log(req.params.email_id)
+    Profile.findOne({ email_address: req.params.email_id })
+        .exec((err, profile) => {
+            console.log(profile._id)
+            Request.find({ faculty: profile._id })
                 .populate('faculty')
                 .lean()
                 .sort({ created: -1 })
