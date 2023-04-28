be9fbfc509aec662c6f86bb7ee6faab532768a58
pranav-159 <cs20btech11018@iith.ac.in>
2023-04-28 10:11:00 +0530

    added delete api and refractored code to handle errors

--- Backend/controller/requestController.js
+++ Backend/controller/requestController.js
@@ -1,19 +1,41 @@
 const Request = require('../model/request');
 
+//get request details
 exports.get_request = (req, res, next) => {
+
     Request.findById(req.params.req_id)
+        .orFail()
         .populate('faculty')
         .lean()
         .exec((err, request) => {
             if (err) {
-                console.log(err);
-                res.status(500).json(err);
-                next(err);
+                // console.log(err);
+                res.status(500).json({ message: "No such request exists" });
+                // next(err);
             }
-            request.faculty_name = request.faculty.username;
-            request.email_address = request.faculty.email_address;
-            delete request.faculty;
-            res.status(200).json(request);
+            else {
+                request.faculty_name = request.faculty.username;
+                request.email_address = request.faculty.email_address;
+                delete request.faculty;
+                res.status(200).json(request);
+            }
         });
+    
+}
+
+//delete the request
+exports.delete_request = (req, res, next) => {
+    Request.findByIdAndDelete(req.params.req_id)
+        .orFail()
+        .exec((err, request) => {
+            if (err) {
+                res.status(500).json({ message : "cannot delete non-existing request" });
+                // next(err);
+            }
+            else {
+                   res.status(200).json({ message: "deleted" });
+            }
+        });
+ 
 }
 
--- Backend/routes/requestroute.js
+++ Backend/routes/requestroute.js
@@ -2,6 +2,7 @@ const express = require('express')
 const requestController = require('../controller/requestController.js')
 const router = express.Router()
 
-router.use('/:req_id', requestController.get_request);
+router.route('/:req_id').get(requestController.get_request);
+router.route('/delete/:req_id').delete(requestController.delete_request);
 
 module.exports = router;
